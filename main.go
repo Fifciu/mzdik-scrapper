fa9c14442845e87ae3bd5223848b8b47a70b45b7
Filip Jędrasik <filip.jdrasik@gmail.com>
2019-01-14 15:07:22 +0100

    Deleted commnets from main.go

--- main.go
+++ main.go
@@ -14,13 +14,5 @@ func main(){
 	router.HandleFunc("/buses/{id}/timetable/{way}", apiGetBusTimetable).Methods("GET");
 	router.HandleFunc("/buses/{id}/timetable/{way}/{dayType}", apiGetBusDailyTimetable).Methods("GET");
 
-	log.Fatal(http.ListenAndServe(":8000", router));
-	/*client, err := dbConnect();
-	if err != nil {
-		log.Fatalf("error initializing app: %v\n", err)
-	}
-	for i := 1; i <= 26; i++ {
-		dbUpdateBusStation(i, client);
-		dbUpdateBusTimetable(i, client);
-	}*/
+	log.Fatal(http.ListenAndServe(":8000", router)); 
 }
